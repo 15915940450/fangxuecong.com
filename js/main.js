b491c89b971d9cef71ee40d33f1f1e6f17b5cab5
15915940450 <15915940450@139.com>
2017-06-14 21:07:14 +0800

    theater

--- js/main.js
+++ js/main.js
@@ -9,6 +9,31 @@ $(function(){
   });
 
   /*
+  *theater
+  */
+  var theater = theaterJS()
+
+  theater
+    .on('type:start, erase:start', function () {
+      // add a class to actor's dom element when he starts typing/erasing
+      var actor = theater.getCurrentActor()
+      actor.$element.classList.add('is-typing')
+    })
+    .on('type:end, erase:end', function () {
+      // and then remove it when he's done
+      var actor = theater.getCurrentActor()
+      actor.$element.classList.remove('is-typing')
+    })
+
+  theater
+    .addActor('theater',{ accuracy: 1, speed: 0.6 })
+
+  theater
+    .addScene('theater:大家好，我是学聪...', 400)
+    .addScene('theater:现居住在深圳', 200, '.', 200, '.', 200, '. ')
+    .addScene(theater.replay)
+
+  /*
   *head
   */
   // return;
