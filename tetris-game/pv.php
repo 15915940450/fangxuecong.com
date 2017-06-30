16d3cec99fca9e6f5e6179f5154d0d6e26994bf4
15915940450 <15915940450@139.com>
2017-06-30 10:46:21 +0800

    f show pv

--- tetris-game/pv.php
+++ tetris-game/pv.php
@@ -1,5 +1,4 @@
 <?php
-  $pageid=$_GET["pageid"];
   //使用面向对象进行数据库的连接，在创建对象的时候就自动的连接数据,密码为空
   $MSQL=new MySQLi('localhost','root','','fangxuec_tetris',3306);
   if($MSQL->connect_errno){
@@ -7,11 +6,18 @@
   }
   $MSQL->set_charset('utf8');
   //============================
-  $sql1="UPDATE pv SET pv=pv+1 WHERE id='$pageid'";
-  $res=$MSQL->query($sql1);
+  if(isset($_GET["pageid"])){
+    $pageid=$_GET["pageid"];
+    $sql1="UPDATE pv SET pv=pv+1 WHERE id='$pageid'";
+    $res=$MSQL->query($sql1);
 
-  $sql2="SELECT * FROM pv WHERE id='$pageid'";
-  $res=$MSQL->query($sql2);
+    $sql2="SELECT * FROM pv WHERE id='$pageid'";
+    $res=$MSQL->query($sql2);
+  }else{
+    $sql2="SELECT * FROM pv";
+    $res=$MSQL->query($sql2);
+  }
+
   $pv=array();
   while($row=$res->fetch_object()){
     $pv[]=$row;
