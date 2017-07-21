c8a935f0ebcc9a2616a30e5bd6c7bcabf5998d69
15915940450 <15915940450@139.com>
2017-07-21 14:42:00 +0800

    can i use class?

--- test/gomoku/main.js
+++ test/gomoku/main.js
@@ -7,9 +7,25 @@ eleFxcGomoku.onclick=function(){
 
 //2017-07-21 周五 10:32 上午
 var FxcGomoku=function(){
-
+  this.nowData=[]
 }
 
 FxcGomoku.prototype={
-  init:function(){}
-};
+  init:function(){
+    for(var i=0;i<15;i++){
+      this.nowData[i]=[];
+      for(var j=0;j<15;j++){
+        this.nowData[i][j]={
+          piece:'black'
+        };
+      }
+    }
+  }, //end of init
+  render:function(){
+
+  }
+};  //end of prototype
+
+var objFxcGomoku=new FxcGomoku();
+objFxcGomoku.init();
+console.log(objFxcGomoku.nowData);
