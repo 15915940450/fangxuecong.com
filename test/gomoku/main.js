e7da49445e3065b8cad2dd1e615d2fac1adcfb60
15915940450 <15915940450@139.com>
2017-07-21 13:02:08 +0800

    fxc gomoku

--- test/gomoku/main.js
+++ test/gomoku/main.js
@@ -1,10 +1,15 @@
-//alert('gomoku');
+/*
+var eleFxcGomoku=document.getElementById('fxc-gomoku');
+eleFxcGomoku.onclick=function(){
+  alert('test');
+};
+*/
 
 //2017-07-21 周五 10:32 上午
-var Gomoku=function(){
+var FxcGomoku=function(){
 
 }
 
-Gomoku.prototype=function(){
+FxcGomoku.prototype={
   init:function(){}
 };
