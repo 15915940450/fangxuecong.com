06518e8daf7021927c66bbda5bae4195b76d112c
15915940450 <15915940450@139.com>
2017-11-23 17:46:43 +0800

    path.resolve()

---
+++ test.js
@@ -0,0 +1,13 @@
+const path=require('path');
+
+//var strBasename=path.basename('C:\\Users\\Administrator\\frontend\\fangxuecong.com\\test.htmll');
+//console.log(strBasename);
+//UsersAdministrator♀rontend♀angxuecong.com
+//var strDirname=path.dirname();
+//console.log(strDirname);
+//var strJoin=path.join('/foo', 'bar', 'baz/asdf', 'quux', '.');
+//console.log(strJoin);
+console.log('===========================================');
+
+var strResolve=path.resolve();
+console.log(strResolve);
