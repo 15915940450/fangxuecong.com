521a6273e55462c506c243b9df659788a8d71c1f
15915940450 <15915940450@139.com>
2019-12-13 15:25:25 +0800

    init gung

--- algorithm/souduk/index.js
+++ algorithm/souduk/index.js
@@ -1,4 +1,29 @@
-class Basic{
+/*
+Fisher–Yates shuffle
+https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+
+
+[1,2,3,4,5,6,7,8].shuffle()
+*/
+Array.prototype.shuffle = function() {
+  //避免修改原数组
+  var input = JSON.parse(JSON.stringify(this));
+
+  for (var i = input.length-1; i >=0; i--) {
+
+    var randomIndex = Math.floor(Math.random()*(i+1));
+    var itemAtIndex = input[randomIndex];
+
+    input[randomIndex] = input[i];
+    input[i] = itemAtIndex;
+  }
+  return (input);
+};
+/*var a=[1,2,3,5];
+var b=a.shuffle();
+console.log(a,b);*/
+
+class Souduk{
   constructor(){
     this.n=-1;  //raf多少次
     this.interval=10; //每幀的間隔
@@ -6,6 +31,9 @@ class Basic{
 
     this.CW=document.documentElement.clientWidth || document.body.clientWidth;
     this.CH4=document.documentElement.clientHeight || document.body.clientHeight;
+
+
+    this.arrGung=[];  //宫数据
   }
 
   //判断动画是否 持续 进行
@@ -14,7 +42,28 @@ class Basic{
   }
 
   init(){
-    
+    //首先生成1，5，9這三個宮，索引是0，4，8
+    this.gung159();
+  }
+
+  gung159(){
+    var f=this;
+    var i,j,arr1_9=[];
+    for(i=0;i<9;i++){
+      f.arrGung[i]=f.arrGung[i] || [];
+      arr1_9.push(i+1);
+      for(j=0;j<9;j++){
+        f.arrGung[i][j]=0;
+      }
+    }
+
+    f.arrGung[0]=arr1_9.shuffle();
+    f.arrGung[4]=arr1_9.shuffle();
+    f.arrGung[8]=arr1_9.shuffle();
+
+    // console.log(arr1_9);  //[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    return f;
   }
 
   // 算法
@@ -45,11 +94,18 @@ class Basic{
   doINeveryframe(){
     var f=this;
     console.log(f.currentStep);
+    f.draw();
+    return f;
+  }
+
+  draw(){
+    var f=this;
+    
     return f;
   }
 
 } //class
 
-var obj=new Basic();
+var obj=new Souduk();
 obj.init();
 obj.solve();
